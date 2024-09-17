57a6e5a1091a44d222f19f9d0f38d5b07a8bd147
Natanael de Jesus Santos <natanael4024716@aluno.ba.senac.br>
2024-09-17 19:15:02 -0300

    Atualização

--- js/detalhes.js
+++ js/detalhes.js
@@ -1,1 +1,11 @@
 var id = parseInt(localStorage.getItem('detalhe'));
+
+var produtos = JSON.parse(localStorage.getItem('produtos'));
+
+var item = produtos.find(produto => produto.id === id);
+
+if (item) {
+    console.log('produto Encontrado', item);
+} else {
+    console.log('Produto não encontrado', item);
+}
